06287c1d4618e9eceb4f7ebbbe6aeb854d66e6f4
ABHINAVGUPTA02 <abhinav.gupta.dev05@gmail.com>
2026-02-18 04:24:52 +0530

    output updated

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -24,7 +24,7 @@ class GraphBuilder():
         user_question = state["messages"]
         input_question = [self.system_prompt] + user_question
         response = self.llm_with_tools.invoke(input_question)
-        return response
+        return {"messages": [response]}
 
     def build_graph(self):
         graph_builder = StateGraph(MessagesState)
