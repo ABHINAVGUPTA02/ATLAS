e71f8b3d255e7dbfa62f90e1a8951a741881e7a0
ABHINAVGUPTA02 <abhinav.gupta.dev05@gmail.com>
2026-02-18 06:17:13 +0530

    workflow updated, tools added

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -7,17 +7,25 @@ from utils.model_loader import MODEL_LOADER
 
 from tools.weather_info_tool import WeatherInfoTool
 from tools.place_search_tool import PlaceSearchTool
-from tools.expense_calculator_tool import CalculatorTool
+from tools.expense_calculator_tool import ExpenseCalculatorTool
 from tools.currency_conversion_tool import CurrencyConverterTool
 
 class GraphBuilder():
-    def __init__(self):
-       self.tools = [
-           WeatherInfoTool(), 
-           PlaceSearchTool(), 
-           CalculatorTool(), 
-           CurrencyConverterTool()
-       ] 
+    def __init__(self, model_provider: str = "groq"):
+       self.model_loader = MODEL_LOADER(model_provider=model_provider)
+       self.llm = self.model_loader.load_llm()       
+       self.tools = []
+       self.weather_tools = WeatherInfoTool()
+       self.place_search_tool = PlaceSearchTool()
+       self.calculator_tool = ExpenseCalculatorTool()
+       self.currency_converter_tool = CurrencyConverterTool()
+
+       self.tools.extend([
+           *self.weather_tools.weather_tool_list,
+           *self.place_search_tool.place_search_tool_list,
+           *self.calculator_tool.expense_calculator_tool_list,
+           *self.currency_converter_tool.currency_converter_tool_list
+       ])
        self.system_prompt = SYSTEM_PROMPT
 
     def agent_function(self, state:MessagesState):
