c7d3f69d1e35caf8cce3900edc7c14d98c4ec54e
ABHINAVGUPTA02 <abhinav.gupta.dev05@gmail.com>
2026-02-18 06:17:28 +0530

    workflow updated, tools added

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -26,6 +26,11 @@ class GraphBuilder():
            *self.calculator_tool.expense_calculator_tool_list,
            *self.currency_converter_tool.currency_converter_tool_list
        ])
+
+       self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+
+       self.graph = None
+       
        self.system_prompt = SYSTEM_PROMPT
 
     def agent_function(self, state:MessagesState):
